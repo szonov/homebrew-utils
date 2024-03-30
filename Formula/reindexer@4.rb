eaf40ebb8249f46c8d7a2a8c290b0b650d865d69
Sergey Zonov <sergey@zonov.com>
2024-03-30 10:02:36 +0700

    First reindexer@4 formula

---
+++ Formula/reindexer@4.rb
@@ -0,0 +1,70 @@
+class Reindexer < Formula
+  env :std
+  desc "is a fast document-oriented in-memory database."
+  homepage "https://github.com/restream/reindexer"
+  url "https://github.com/Restream/reindexer/archive/v4.14.0.zip"
+  version "4.14.0"
+  sha256 "3b8bed4734aa4eb7452afded2a2596a239bd36b4056b4ba5ea6e4194857beb7f"
+
+  head "https://github.com/restream/reindexer.git"
+
+  depends_on "cmake" => :build
+  depends_on "leveldb"
+
+  def install
+
+    mkdir "build"
+    cd "build" do
+      system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".."
+      system "make", "-j8", "reindexer_server", "reindexer_tool", "install"
+    end
+
+    mkdir "#{var}/reindexer"
+    mkdir "#{var}/log/reindexer"
+
+    inreplace "#{buildpath}/build/cpp_src/cmd/reindexer_server/contrib/config.yml" do |s|
+      s.gsub! "/var/lib/reindexer", "#{var}/reindexer"
+      s.gsub! "/var/log/reindexer", "#{var}/log/reindexer"
+      s.gsub! "user:", "# user:"
+    end
+
+    # Copy configuration files
+    etc.install "#{buildpath}/build/cpp_src/cmd/reindexer_server/contrib/config.yml" => "reindexer.conf"
+  end
+
+  def plist; <<-EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/reindexer_server</string>
+            <string>--config</string>
+            <string>#{etc}/reindexer.conf</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/reindexer/reindexer.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/reindexer/reindexer.log</string>
+        </dict>
+    </plist>
+    EOS
+  end
+
+  def caveats; <<-EOS
+    The configuration file is available at:
+      #{etc}/reindexer.conf
+    The database itself will store data at:
+      #{var}/reindexer/
+  EOS
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# homebrew-utils
+# Homebrew Utils
+
 This is a repository with utilities that I use for personal needs and which do not meet the requirements for a centralized homebrew repository.
+
+## Usage
+
+Tap the repo with:
+
+```
+brew tap szonov/utils
+```
+
+You can then install any formula.
+
+## Formula listing
+
+- Reindexer v4 `brew install reindexer@4`
+
+## License
+
+MIT, see LICENSE file.
